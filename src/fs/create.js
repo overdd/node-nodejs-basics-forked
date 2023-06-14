1d78a8144677cb6ce73a8a946b050584d2c49c43
overdd <lmbib3a4@duck.com>
2023-06-14 21:01:52 +0200

    nodejs basics create.js is ready

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, "files", "fresh.txt");
+    const data = "I am freash and young";
+    fs.access(pathToFile, fs.constants.F_OK, (error) => {
+        if (!error) {
+            throw new Error(`FS operation failed`);
+        }
+    });
+    fs.writeFile(pathToFile, data, (error) => {
+        if (error) {
+            throw new Error(error);
+        }
+    });
 };
 
 await create();
