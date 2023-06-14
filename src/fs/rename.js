68381d594a87d66907a4b623057472866da87801
overdd <lmbib3a4@duck.com>
2023-06-14 22:06:13 +0200

    nodejs basics rename.js is ready

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const pathBeforeRename = path.join(__dirname, "files", "wrongFilename.txt");
+    const pathAfterRename = path.join(__dirname, "files", "properFilename.md");
+    
+    validateBeforeRename(pathBeforeRename);
+    validateAfterRename(pathAfterRename);
+
+    fs.rename(pathBeforeRename, pathAfterRename, (error) => {
+        if (error) {
+            throw new Error(error);
+        }
+    })
 };
+
+const validateBeforeRename = async(path) => {
+    fs.access(path, fs.constants.F_OK, (error) => {
+        if (error) {
+            throw new Error(`FS operation failed`);
+        }
+    });
+}
+
+const validateAfterRename = async(path) => {
+    fs.access(path, fs.constants.F_OK, (error) => {
+        if (!error) {
+            throw new Error(`FS operation failed`);
+        }
+    });
+}
 
 await rename();
