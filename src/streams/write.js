833cd43c1958204407278f0c3a66f5efa0001902
overdd <lmbib3a4@duck.com>
2023-06-15 15:29:41 +0200

    nodejs basics src/streams is ready

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const pathToWrite = path.join(__dirname, "files", "fileToWrite.txt");
+
+    validatePathToFile(pathToWrite);
+
+    const writableStream = fs.createWriteStream(pathToWrite);
+    process.stdin.pipe(writableStream); // run the script and type data in console - it'll appear in fileToWrite.txt
 };
+
+const validatePathToFile = async(path) => {
+    fs.access(path, fs.constants.F_OK, (error) => {
+        if (error) {
+            throw new Error(`FS operation failed`);
+        }
+    });
+}
 
 await write();
