ca8cf23bc9af1a2aca627d645ddcea82fdf67e04
overdd <lmbib3a4@duck.com>
2023-06-17 20:53:29 +0200

    nodejs basics src/cp is ready but not sure if it is correct

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -5,8 +5,10 @@ console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+    if (chunkStringified.includes("CLOSE")) process.exit(0);
+    process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
+
 };
 
-process.stdin.on('data', echoInput);
+process.stdin.on("data", echoInput);
+
